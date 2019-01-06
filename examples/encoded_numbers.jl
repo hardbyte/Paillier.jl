0476463006bf3609c083dfbae93c8639354d5274
Brian Thorne <brian@thorne.link>
2019-01-06 17:23:55 +1100

    Add an example script that uses a custom encoding base.

--- examples/encoded_numbers.jl
+++ examples/encoded_numbers.jl
@@ -5,22 +5,22 @@ This example uses a custom Encoding.
 
 """
 keysize = 2048
-base = 16
+base = 64
 
 publickey, privatekey = generate_paillier_keypair(keysize)
 encoding = Encoding(Float16, publickey, base)
 
-a = 2000
-b = 100
+a = 2000.0
+b = 100.0
 enc1 = encode_and_encrypt(a, encoding)
 enc2 = encode_and_encrypt(b, encoding)
 
-@info "Decrypt $a: $(decrypt_and_decode(privatekey, enc1))"
-@info decrypt_and_decode(privatekey, enc2)
-
 enc3 = decrypt_and_decode(privatekey, enc1 + enc2)
-@info enc3
+println("Adding encrypted numbers: $a + $b = $enc3")
 
 enc4 = decrypt_and_decode(privatekey, enc1 - 20.0)
-@info enc4
+println("Subtract a constant: $a - 20.0 = $enc4")
+
+# enc5 = decrypt_and_decode(privatekey, 3*enc1)
+# println("Multiplication: 3$a = $enc5")
 
