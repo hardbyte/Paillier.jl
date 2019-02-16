a800cf1e6bbc867e6eb06dc0b484a98a3b558205
Brian Thorne <brian@thorne.link>
2019-02-16 14:02:39 +1100

    Revert making the rng global

--- src/utilities.jl
+++ src/utilities.jl
@@ -1,6 +1,4 @@
 
-rng = RandomDevice()
-
 function n_bit_random_number(len::Number)
     max_n = ( BigInt(1) << len ) - 1
     if len > 2
@@ -17,6 +15,7 @@ function nbit_prime_of_size(n_bits)
 end
 
 function random_lt_n(n::BigInt)
+    rng = RandomDevice()
     return rand(rng, big.(1:n))
 end
 
